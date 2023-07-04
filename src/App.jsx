41b87a8a26113f8b194d293abc7b8690b8dfe51d
Josue Choque <89545612+0seaS@users.noreply.github.com>
2023-07-04 13:32:21 -0400

    Maquetado basico Html

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Clima from './components/Clima'
 import './App.css'
 
 function App() {
@@ -6,6 +6,22 @@ function App() {
 
   return (
     <>
+      <header>
+        <nav>
+          <ul>
+            <li>Weather app</li>
+            <li><button><i class='bx bx-search-alt-2'></i></button><input type="text" /></li>
+            <li>
+              <label className="switch">
+                <input type="checkbox"/>
+                <span className="slider round"></span>
+              </label>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Clima></Clima>
+      
       
     </>
   )

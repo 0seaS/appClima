a729cc2f3327f6608e2bab9af20a0bd76f83d63a
Josue Choque <89545612+0seaS@users.noreply.github.com>
2023-07-05 13:12:49 -0400

    Se agrega la funcionalidad

--- src/components/Clima.jsx
+++ src/components/Clima.jsx
@@ -3,53 +3,71 @@ import axios, { Axios } from "axios"
 import { useState, useEffect } from "react"
 const Clima = () => {
 
-    let latitude = 0
-    let longitude = 0
 
-    navigator.geolocation.getCurrentPosition((pos) => {
-        const crd = pos.coords;
-        // console.log("Your current position is:");
-        // console.log(`Latitude : ${crd.latitude}`);
-        // console.log(`Longitude: ${crd.longitude}`);
-        // console.log(`More or less ${crd.accuracy} meters.`);
+    const [latitude, setLatitude] = useState(0)
+    const [longitude, setLongitude] = useState(0)
 
-        latitude = crd.latitude
-        longitude = crd.longitude
-    })
+    //let latitude = 0
+    //let longitude = 0
 
-    const [weather, setWeather]= useState({})
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            const crd = pos.coords;
+            // console.log("Your current position is:");
+            // console.log(`Latitude : ${crd.latitude}`);
+            // console.log(`Longitude: ${crd.longitude}`);
+            // console.log(`More or less ${crd.accuracy} meters.`);
+            
+            setLatitude(crd.latitude)
+            setLongitude(crd.longitude)
+        })
+    },[])
+
+    const [allWeather, setAllWeather] = useState({})
+    const [weather, setWeather] = useState({})
+    const [isCelsius, setIsCelsius] = useState(false)
+
 
     useEffect(() => {
         axios
         .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=sp&appid=510f0eaa72c51a328e4614b9e40d2fb5`)
         .then(res => {
             //console.log(res.data.weather[0]?.description)
-            setWeather(res.data.weather[0])
+            setAllWeather(res?.data)
+            setWeather(res.data?.weather[0])
+
         })
         .catch(error => console.log(error));
     },[])
 
+    console.log(allWeather)
     console.log(weather)
 
+    function changeUnits(){
+        setIsCelsius(!isCelsius)
+    }
+
+    /*`/iconos/${allWeather?.weather[0].icon}.svg`*/
+    /*allWeather?.weather[0].description*/
     return(
         <>
             <section>
                 <div className="weather-card">
-                    <div className="card-head"><span>35°</span><img src="/iconos/01n.svg" alt="" /></div>
+                    <div className="card-head"><span>{isCelsius ? Math.round(allWeather.main?.temp - 273.15) : Math.round(allWeather.main?.temp)}°</span><img src={`/iconos/${weather?.icon}.svg`} alt="" /></div>
                     <div className="card-body">
                         <ul>
-                            <li>Viento</li>
-                            <li>Nuves</li>
-                            <li>Precion</li>
+                            <li>Viento: {allWeather.wind?.speed} m/s</li>
+                            <li>Nuves: {allWeather.clouds?.all}%</li>
+                            <li>Precion: {allWeather.main?.pressure} hPa</li>
                         </ul>
                     </div>
                     <div className="card-footer">
-                        <span>Ubicacion, Ciudad</span>
-                        <span>{weather.description}</span>
+                        <span>{allWeather.name}, {allWeather.sys?.country ? allWeather.sys?.country : "Global"}</span>
+                        <span>{weather.description /*allWeather.weather[0]?.description*/}</span>
                     </div>
                 </div>
 
-                <button className="btn-change-units">cambiar a F</button>
+                <button className="btn-change-units" onClick={changeUnits}>{isCelsius ? "Cambiar a °F" : "Cambiar a °C"}</button>
             </section>
         </>
     )
